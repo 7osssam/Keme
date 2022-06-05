69e980af7413480924a43cb36040da81f0be0e2e
mahmoudashry98 <83143927+mahmoudashry98@users.noreply.github.com>
2022-06-05 11:40:13 +0200

    bottom_nav_bar

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,9 +1,12 @@
+
 import 'package:flutter/material.dart';
 import 'package:keme/places_tab.dart';
+import 'package:keme/profile_tab.dart';
 import 'package:keme/setting_tab.dart';
+import 'package:navigation_action_bar/navigation_action_bar.dart';
+
 import 'camera_tab.dart';
 import 'map_tab.dart';
-//import 'package:floating_bottom_bar/animated_bottom_navigation_bar.dart';
 
 class HomeScreen extends StatefulWidget {
   static const String routeName = 'HomeScreen';
@@ -19,100 +22,65 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: tabs[currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: currentIndex,
-        onTap: (index) {
-          currentIndex = index;
-          setState(() {});
-        },
-        items: const [
-          BottomNavigationBarItem(
-              icon: Icon(Icons.place, color: Colors.orangeAccent,),
-              label: "Places"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.map_outlined, color: Colors.orangeAccent),
-              label: "Map"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.camera, color: Colors.orangeAccent),
-              label: "Camera"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.settings, color: Colors.orangeAccent),
-              label: "Setting"),
+      bottomNavigationBar: NavigationActionBar(
+        backgroundColor: Color.fromRGBO(34, 33, 38, 1),
+        context: context,
+        scaffoldColor: Color.fromRGBO(34, 33, 38, 1),
+        index: 0,
+        subItems: [
+          NavBarItem(iconData: Icons.home, size: 25),
+          NavBarItem(iconData: Icons.search, size: 25),
+          NavBarItem(iconData: Icons.camera_alt, size: 25),
         ],
+        mainIndex: 2,
+        items: [
+          NavBarItem(iconData: Icons.place_sharp, size: 30),
+          NavBarItem(iconData: Icons.map_outlined, size: 30),
+          NavBarItem(iconData: Icons.add, size: 60),
+          NavBarItem(iconData: Icons.person, size: 30),
+          NavBarItem(iconData: Icons.settings, size: 30),
+        ],
+        onTap: (index) {
+          setState(() {
+           currentIndex = index.toInt();
+          });
+        },
       ),
-      // bottomNavigationBar: AnimatedBottomNavigationBar(
-      //         bottomBarItems: [
-      //           BottomBarItemsModel(
-      //               icon: const Icon(Icons.home, size: example.Dimens.iconNormal),
-      //               iconSelected: const Icon(Icons.home, color: AppColors.cherryRed, size: example.Dimens.iconNormal),
-      //               title: example.Strings.home,
-      //               dotColor: example.AppColors.cherryRed,
-      //               onTap: () =>log('Home'),
-      //           ),
-      //           const BottomBarItemsModel(
-      //             icon: Icon(Icons.search, size: example.Dimens.iconNormal),
-      //             iconSelected: Icon(Icons.search, color: AppColors.cherryRed, size: example.Dimens.iconNormal),
-      //             title: example.Strings.search,
-      //             dotColor: example.AppColors.cherryRed,
-      //             onTap: () =>log('Search'),
-      //           ),
-      //           const BottomBarItemsModel(
-      //             icon: Icon(Icons.person, size: example.Dimens.iconNormal),
-      //             iconSelected: Icon(Icons.person, color: AppColors.cherryRed, size: example.Dimens.iconNormal),
-      //             title: example.Strings.person,
-      //             dotColor: example.AppColors.cherryRed,
-      //             onTap: () =>log('Person'),
-      //           ),
-      //           const BottomBarItemsModel(
-      //             icon: Icon(Icons.settings, size: example.Dimens.iconNormal),
-      //             iconSelected: Icon(Icons.settings, color: AppColors.cherryRed, size: example.Dimens.iconNormal),
-      //             title: example.Strings.settings,
-      //             dotColor: example.AppColors.cherryRed,
-      //             onTap: () =>log('Settings'),
-      //           ),
-      //         ],
-      //         bottomBarCenterModel: BottomBarCenterModel(
-      //           centerBackgroundColor: example.AppColors.cherryRed,
-      //           centerIcon: const FloatingCenterButton(
-      //             child: Icon(
-      //               Icons.add,
-      //               color: AppColors.white,
-      //             ),
-      //           ),
-      //           centerIconChild: [
-      //             FloatingCenterButtonChild(
-      //               child: const Icon(
-      //                 Icons.home,
-      //                 color: AppColors.white,
-      //               ),
-      //               onTap: () {},
-      //             ),
-      //             FloatingCenterButtonChild(
-      //               child: const Icon(
-      //                 Icons.home,
-      //                 color: AppColors.white,
-      //               ),
-      //               onTap: () {},
-      //             ),
-      //             FloatingCenterButtonChild(
-      //               child: const Icon(
-      //                 Icons.home,
-      //                 color: AppColors.white,
-      //               ),
-      //               onTap: () {},
-      //             ),
-      //           ],
-      //         ),
-      //       ),
+      // bottomNavigationBar: BottomNavigationBar(
+      //   currentIndex: currentIndex,
+      //   onTap: (index) {
+      //     currentIndex = index;
+      //     setState(() {});
+      //   },
+      //   items: const [
+      //     BottomNavigationBarItem(
+      //         icon: Icon(Icons.place, color: Colors.orangeAccent,),
+      //         label: "Places"),
+      //     BottomNavigationBarItem(
+      //         icon: Icon(Icons.map_outlined, color: Colors.orangeAccent),
+      //         label: "Map"),
+      //     BottomNavigationBarItem(
+      //         icon: Icon(Icons.camera, color: Colors.orangeAccent),
+      //         label: "Camera"),
+      //     BottomNavigationBarItem(
+      //         icon: Icon(Icons.settings, color: Colors.orangeAccent),
+      //         label: "Setting"),
+      //   ],
+      // ),
 
     );
   }
+
 
   var tabs = [
     PlacesTab(),
     MapTab(),
     CameraTab(),
+    ProfileTab(),
     SettingTab(),
   ];
 }
+
+
+
 
---
+++ lib/profile_tab.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ProfileTab extends StatelessWidget {
+  static String routeName = 'ProfileTab';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
