c822f5f5930795757ba7047db3f19af662285f5d
Mahmoud <modygaber2018@gmail.com>
2022-06-20 18:15:24 +0200

    modifiy

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:keme/filter_tab.dart';
+import 'package:keme/qr_tab.dart';
 import 'package:keme/my_theme.dart';
 import 'package:keme/place_360_degree.dart';
 import 'package:keme/place_details.dart';
-import 'package:keme/screens/reset_password.dart';
-import 'package:keme/screens/signin_screen.dart';
-import 'package:keme/screens/signup_screen.dart';
+import 'package:keme/setting_tab.dart';
 import 'ar_tour_tab.dart';
 import 'home_screen.dart';
 
@@ -24,16 +22,14 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: "Keme",
       routes: {
-        SignInScreen.routeName: (buildContext) => SignInScreen(),
-        SignUpScreen.routeName: (buildContext) => SignUpScreen(),
-        ResetPassword.routeName: (buildContext) => ResetPassword(),
         HomeScreen.routeName: (buildContext) => HomeScreen(),
         PlaceDetails.routeName: (buildContext) => PlaceDetails(),
-        FilterTab.routeName: (buildContext) => FilterTab(),
+        QRViewExample.routeName: (buildContext) => FilterTab(),
         ArTourTab.routeName: (buildContext) => ArTourTab(),
         Place360Degree.routeName: (buildContext) => Place360Degree(),
+        SettingTab.routeName: (buildContext) => SettingTab(),
       },
-      initialRoute: SignInScreen.routeName,
+      initialRoute: HomeScreen.routeName,
       theme: MyThemeData.DarkTheme,
     );
   }
--- lib/profile_tab.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ProfileTab extends StatelessWidget {
-  static String routeName = 'ProfileTab';
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
-  }
-}
